73eb2c83d4bf74a0236ecbdea28ff27c88fcb2e0
Jose Henrique <jh4832184@gmail.com>
2024-08-24 14:53:54 -0300

    first commit

---
+++ src/model/Person.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Person implements Serializable {
+    private long serialVersionUID;
+    private String name;
+    private String email;
+
+    public Person(String name, String email) {
+        this.serialVersionUID = 1L;
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+        return Objects.equals(email, person.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(email);
+    }
+
+    @Override
+    public String toString() {
+        return " {" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
